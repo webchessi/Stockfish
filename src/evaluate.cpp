19551e98e19987553f3ea4fe8e117901c4101a64
webchessi <webchessi@web.de>
2013-05-20 18:46:41 +0200

    Space_Eval
    Front of pawns safe

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1094,11 +1094,14 @@ Value do_evaluate(const Position& pos, Value& margin) {
     behind |= (Us == WHITE ? behind >>  8 : behind <<  8);
     behind |= (Us == WHITE ? behind >> 16 : behind << 16);
 
+	Bitboard Front = pos.pieces(Us, PAWN);
+    Front |= (Us == WHITE ? behind <<  8 : behind >>  8);
+
     // Since SpaceMask[Us] is fully on our half of the board
     assert(unsigned(safe >> (Us == WHITE ? 32 : 0)) == 0);
 
     // Count safe + (behind & safe) with a single popcount
-    return popcount<Full>((Us == WHITE ? safe << 32 : safe >> 32) | (behind & safe));
+    return popcount<Full>((Us == WHITE ? safe << 32 : safe >> 32) | (behind & safe) | (Front & safe));
   }
 
 
